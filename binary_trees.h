49940747510df9f361833332bb7c0378527a21f2
bion-slmn <bionsol25@gmail.com>
2023-08-29 23:49:28 -0700

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,5 @@ int binary_tree_heigh(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
